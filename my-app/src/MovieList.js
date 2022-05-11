fd7c347c30f9a688c0c801e98af5d2fea06ab566
kroonfilip <kroon.filip@gmail.com>
2022-05-11 18:31:40 +0200

    test

--- my-app/src/MovieList.js
+++ my-app/src/MovieList.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 import Movie from './Movie';
 
@@ -25,14 +25,11 @@ export default function MovieList() {
         grade: 3
     }]);
 
-    //useEffect(() => {
-      //  setMovies(movies)
-    //}, [])
-
+    
     const titleRef = useRef();
     const gradeRef = useRef();
 
-    function addMovie(event) {
+    function addMovie(event){
         if (titleRef.current.value !== "" && gradeRef.current.value > 0){
             const newId = movies.length > 0 ? movies[movies.length - 1].id + 1: 1;
             setMovies([...movies, {
@@ -51,25 +48,41 @@ export default function MovieList() {
     function deleteMovie(id) {
         setMovies(movies.filter((movie) => movie.id !== id));
     }
+    useEffect(() => {
+        setMovies(movies)
+      }, [])
+  
 
-    function handleSort(order) {
-       if (order === 1){
-        console.log(1)
-        movies.sort((a, b) => (a.title.toLowerCase() > b.title.toLowerCase()) ? 1 : -1);
-        return movies.map(movie => <Movie key={movie.id} movie={movie} deleteMovie={deleteMovie}/>)
-       } else{
-          movies.sort((a, b) => (a.grade < b.grade) ? 1 : -1)
-          return movies.map(movie => <Movie key={movie.id} movie={movie} deleteMovie={deleteMovie}/>)
-       } 
-    
+    function handleSortAlpha() {
+       const sorted = [...movies].sort((a,b) => {
+           return (a.title.toLocaleLowerCase() > b.title.toLocaleLowerCase())? 1: -1
+       })
+       setMovies(sorted)
     }
+
+    function handleSortNumeric() {
+        const sortedNr = [...movies].sort((a, b) => {
+            return (a.grade < b.grade)? 1: -1
+        })
+        setMovies(sortedNr)
+    };
     
-    let moviesComponents = movies.map(movie => <Movie key={movie.id} movie={movie} deleteMovie={deleteMovie}/>);
-        
+    //} else{
+          //movies.sort((a, b) => (a.grade < b.grade) ? 1 : -1)
+         // return movies.map(movie => <Movie key={movie.id} movie={movie} deleteMovie={deleteMovie}/>)
+       //} 
+    
+    
+    
+    let moviesComponents = movies.map(movie => {
+    return <Movie key={movie.id} movie={movie} deleteMovie={deleteMovie}/>;
+    
+   })
+    
     
     return (
         <div className="row container-fluid">
-            <form onSubmit = {addMovie}>
+            <form action ="#"onSubmit = {addMovie}>
             <label htmlFor="title">Titel:</label>
             <input id="title" className="form-control" ref={titleRef} />
             <div className="form-group">
@@ -84,19 +97,26 @@ export default function MovieList() {
                         </select>
                     </div>
                     <div className="form-group mt-2">
-                        <button id="save-movie" className="btn btn-success">
+                        <button type ="submit" value ="Submit" id="save-movie" className="btn btn-success">
                             Spara film
                         </button>
+                            
+                        
                     </div>
                     <hr></hr>
                     <h2>Mina filmer</h2>
                     <ul className="list-group">
-                {moviesComponents}
+                    
+                   {moviesComponents}
+                    
             </ul>
             </form>
             
-            <button id="OrderByAlpha" onClick={()=> {this.handleSort(1)}}className="btn btn-primary">Alfabetisk ordning</button>
-            <button id="OrderByGrade" onClick={()=> {handleSort(2)}}className="btn btn-primary">Betygsordning</button>
+            <button id="OrderByAlpha" onClick= {handleSortAlpha}className="btn btn-primary">Alfabetisk ordning</button>
+            <button id="OrderByGrade" onClick= {handleSortNumeric}className="btn btn-primary">Betygsordning</button>
         </div>
     )
+    
+    
+    
 }
